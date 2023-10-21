d8ae67d9716b15f548df5068ab6b1fd72cee704d
Mohamed Aziz Hlel <med.aziz.hlel@gmail.com>
2023-10-21 18:56:20 +0100

    Initial Commit

---
+++ src/test/java/de/tekup/tunirent/TuniRentApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.tunirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuniRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
